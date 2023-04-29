896337afa74509e011218dff7e7b38e9bd524d61
amachad4 <amachad4@masonlive.gmu.edu>
2023-04-29 13:20:32 -0400

    Run prettier on entire project

--- app/routes/createTodoItem.tsx
+++ app/routes/createTodoItem.tsx
@@ -1,20 +1,20 @@
-import { ActionArgs, redirect } from "@remix-run/node";
+import { ActionArgs, redirect } from '@remix-run/node';
 
 export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
   const body = Object.fromEntries(formData.entries());
-  console.log("body: ", body);
+  console.log('body: ', body);
   const createTodoItemObj = {
     ...body,
-    created_at: new Date().toJSON(),
+    created_at: new Date().toJSON()
   };
-  const rawResponse = await fetch("http://localhost:5000/api/activities", {
-    method: "POST",
+  const rawResponse = await fetch('http://localhost:5000/api/activities', {
+    method: 'POST',
     body: JSON.stringify(createTodoItemObj),
     headers: {
-      "Content-Type": "application/json",
-    },
+      'Content-Type': 'application/json'
+    }
   });
   const content = await rawResponse.json();
-  return redirect("/todo");
+  return redirect('/todo');
 }
