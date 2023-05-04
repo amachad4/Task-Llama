306bf3cbb96fd06ac246f07516235fc11c7f9794
amachad4 <amachad4@masonlive.gmu.edu>
2023-05-04 00:59:31 -0400

    test lint staged again

--- app/route_components/CreateTaskForm.tsx
+++ app/route_components/CreateTaskForm.tsx
@@ -6,7 +6,7 @@ import { FetcherState, Route } from '~/types/constants';
 
 export default function CreateTaskForm() {
   const fetcher = useFetcher();
-  let testingLinterAgainAndAgain = 101;
+  let testingLinterAgainAndAgain = 102;
 
   let errors: CreateTaskErrorsObject | undefined = fetcher.data?.formErrors;
 
