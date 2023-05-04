c0d9066b3fb40782c8f19b74e4fbdf39b2f9b253
amachad4 <amachad4@masonlive.gmu.edu>
2023-05-03 23:45:50 -0400

    Remove unused import statement

--- app/route_components/NavBar.tsx
+++ app/route_components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { NavLink, useLocation } from '@remix-run/react';
-import { Button, Icon, Menu, Segment } from 'semantic-ui-react';
+import { Icon, Menu, Segment } from 'semantic-ui-react';
 import CreateTaskForm from './CreateTaskForm';
 import TaskLlamaModal from './TaskLlamaModal';
 import { useState } from 'react';
