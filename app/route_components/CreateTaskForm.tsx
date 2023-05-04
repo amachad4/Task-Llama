59b32fbeb27b9e9d6c3898f61e39bb7f40b72272
amachad4 <amachad4@masonlive.gmu.edu>
2023-05-03 23:31:34 -0400

    Install lint stage dev dependency

--- app/route_components/CreateTaskForm.tsx
+++ app/route_components/CreateTaskForm.tsx
@@ -1,93 +1,95 @@
 import { Button, Form, Input, Label } from 'semantic-ui-react';
 import { useFetcher } from '@remix-run/react';
 import { useEffect, useState } from 'react';
-import { CreateTaskErrorsObject } from '~/types/types';
+import type { CreateTaskErrorsObject } from '~/types/types';
 import { FetcherState, Route } from '~/types/constants';
 
 export default function CreateTaskForm() {
-  const fetcher = useFetcher();
+    const fetcher = useFetcher();
 
-  let errors: CreateTaskErrorsObject | undefined = fetcher.data?.formErrors;
+    let errors: CreateTaskErrorsObject | undefined = fetcher.data?.formErrors;
 
-  const [errorsState, setErrorsState] = useState(errors);
+    const [errorsState, setErrorsState] = useState(errors);
 
-  useEffect(() => {
-    if (errors && Object.keys(errors).length) setErrorsState(errors);
-  }, [errors]);
+    useEffect(() => {
+        if (errors && Object.keys(errors).length) setErrorsState(errors);
+    }, [errors]);
 
-  // TODO: pass in options as a prop, see route_components/Form.tsx
-  // TODO: Fix error bump
-  // TODO: Add a better date picker
+    // TODO: pass in options as a prop, see route_components/Form.tsx
+    // TODO: Fix error bump
+    // TODO: Add a better date picker
 
-  return (
-    <fetcher.Form
-      className='ui form'
-      method='post'
-      action={`${Route.CreateTask}`}
-    >
-      <Form.Field error={errorsState?.titleError}>
-        <label>Task Title:</label>
-        <Input
-          placeholder='Task Title'
-          name='title'
-          type='text'
-          onChange={() => {
-            if (errorsState) setErrorsState(undefined);
-          }}
-        />
-        {errorsState?.titleError && (
-          <Label basic color='red' pointing>
-            Please enter a Title
-          </Label>
-        )}
-      </Form.Field>
-      <Form.Field error={errorsState?.deadlineError}>
-        <label>Task Deadline:</label>
-        <Input
-          placeholder='Task Deadline'
-          name='deadline'
-          type='date'
-          onChange={() => {
-            if (errorsState) setErrorsState(undefined);
-          }}
-        />
-        {errorsState?.deadlineError && (
-          <Label basic color='red' pointing>
-            Please select a date!
-          </Label>
-        )}
-      </Form.Field>
-      <div className='flex flex-row items-center mb-5'>
-        <Form.Field
-          label='Task category'
-          control='select'
-          name='category_lkp_id'
-          error={errorsState?.categoryError}
-          className='w-1/2'
-          onChange={() => {
-            if (errorsState) setErrorsState(undefined);
-          }}
+    return (
+        <fetcher.Form
+            className='ui form'
+            method='post'
+            action={`${Route.CreateTask}`}
         >
-          <option value='' selected aria-readonly>
-            Select a Category
-          </option>
-          <option value='1'>Education</option>
-          <option value='2'>Fitness</option>
-        </Form.Field>
-        {errorsState?.categoryError && (
-          <Label basic color='red' pointing='left'>
-            Please select a Category!
-          </Label>
-        )}
-      </div>
+            <Form.Field error={errorsState?.titleError}>
+                <label>Task Title:</label>
+                <Input
+                    placeholder='Task Title'
+                    name='title'
+                    type='text'
+                    onChange={() => {
+                        if (errorsState) setErrorsState(undefined);
+                    }}
+                />
+                {errorsState?.titleError && (
+                    <Label basic color='red' pointing>
+                        Please enter a Title
+                    </Label>
+                )}
+            </Form.Field>
+            <Form.Field error={errorsState?.deadlineError}>
+                <label>Task Deadline:</label>
+                <Input
+                    placeholder='Task Deadline'
+                    name='deadline'
+                    type='date'
+                    onChange={() => {
+                        if (errorsState) setErrorsState(undefined);
+                    }}
+                />
+                {errorsState?.deadlineError && (
+                    <Label basic color='red' pointing>
+                        Please select a date!
+                    </Label>
+                )}
+            </Form.Field>
+            <div className='flex flex-row items-center mb-5'>
+                <Form.Field
+                    label='Task category'
+                    control='select'
+                    name='category_lkp_id'
+                    error={errorsState?.categoryError}
+                    className='w-1/2'
+                    onChange={() => {
+                        if (errorsState) setErrorsState(undefined);
+                    }}
+                >
+                    <option value='' selected aria-readonly>
+                        Select a Category
+                    </option>
+                    <option value='1'>Education</option>
+                    <option value='2'>Fitness</option>
+                </Form.Field>
+                {errorsState?.categoryError && (
+                    <Label basic color='red' pointing='left'>
+                        Please select a Category!
+                    </Label>
+                )}
+            </div>
 
-      <Button
-        type='submit'
-        positive
-        icon='add'
-        content='Add Task'
-        loading={fetcher.state !== FetcherState.Idle.toLocaleLowerCase()}
-      />
-    </fetcher.Form>
-  );
+            <Button
+                type='submit'
+                positive
+                icon='add'
+                content='Add Task'
+                loading={
+                    fetcher.state !== FetcherState.Idle.toLocaleLowerCase()
+                }
+            />
+        </fetcher.Form>
+    );
 }
